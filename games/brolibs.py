e84e12662f80eab7eb9b4b7dd1beefc3222978bb
MSyaekhoni <syaekhonimuhammad@gmail.com>
2025-01-17 06:46:48 +0700

    first commit

---
+++ games/brolibs.py
@@ -0,0 +1,26 @@
+import socket
+from time import sleep
+
+PC_NAME = socket.gethostname()
+
+def welcome_message():
+    style = "*" * (len(PC_NAME) + 6)
+    print(f"\n{style}")
+    print(f"** {PC_NAME} **")
+    print(f"{style}\n")
+
+def exit_program():
+    print("Program akan dihentikan...")
+    sleep(1)
+    print('3...')
+    sleep(1)
+    print('2...')
+    sleep(1)
+    print('1...')
+    sleep(1)
+    print('Program berhasil berhenti.')
+    exit()
+
+if __name__ == '__main__':
+    welcome_message()
+    exit_program()
