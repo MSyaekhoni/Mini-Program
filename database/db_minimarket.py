efc36873ba7fd0dfcb17009f39a9a3cf3907f66e
MSyaekhoni <syaekhonimuhammad@gmail.com>
2025-01-19 22:08:03 +0700

    menambahkan file database python

---
+++ database/db_minimarket.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '',
+    database = 'db_mini_market'
+)
+
+def insert_item(kode_barang, nama_barang, harga_barang, stok_barang):
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO tbl_barang (kode_barang, nama_barang, harga_barang, stok_barang) VALUES (%s, %s, %s, %s)", (kode_barang, nama_barang, harga_barang, stok_barang))
+    db.commit()
+    if cursor.rowcount > 0:
+        print('\nData berhasil disimpan!\n')
+    else:
+        print('\nData gagal tersimpan!\n')
